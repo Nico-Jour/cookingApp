63a2f11df4ef3d7fab013fd1d5a538de1edb6ac5
Nico-Jour <nicolas.journet@outlook.com>
2022-03-03 15:21:40 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# cookingApp
-Projet d'étude en React - Il permet de rechercher des recettes via une API et d'en consulter les informations principales
--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Link} from "react-router-dom"
-import logo from "../Assets/Logo.png"
-
-export default function Logo() {
-  return (
-    
-  <div><Link to="/cookingApp"><img src={logo} className="logo" alt="logo" /></Link></div>
-  )}
